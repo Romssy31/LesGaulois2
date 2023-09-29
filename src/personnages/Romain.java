ad0c2e0beb8230a2812f793fefe384c174572b85
Roman Begorre <bgr4435a@ens.fsi>
2023-09-29 09:40:17 +0200

    Romain question a

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,26 +3,35 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-		public String getNom() {
-			return nom;
+
+	public String getNom() {
+		return nom;
 	}
-		public void parler(String texte) {
-			System.out.println(prendreParole() + "« " + texte + "»");
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
-		private String prendreParole() {
-			return "Le romain " + nom + " : ";
+
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
 	}
-		public void recevoirCoup(int forceCoup) {
-			force -= forceCoup;
-			if (force > 0) {
-				parler("Aïe");
-			} else {
-				parler("J'abandonne...");
-			}
+
+	public void recevoirCoup(int forceCoup) {
+		assert testForcePositive();
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
 		}
+	}
+
+	private boolean testForcePositive() {
+		return force >= 0;
+	}
 }
